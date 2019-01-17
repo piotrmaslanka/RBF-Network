99c38c2c4dafb4cdd768277521f607235f553d9c
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2019-01-17 01:49:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,6 @@ from distutils.core import setup
 setup(name='RBF-Network',
       packages=['RBFN'],
       url='https://github.com/oarriaga/RBF-Network',
-      author='Octavo Arriaga')
+      requires=['numpy'],
+      author='Octavo Arriaga',
+     author_email='arriaga.camargo@gmail.com')
